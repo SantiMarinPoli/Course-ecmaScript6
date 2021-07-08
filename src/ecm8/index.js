19b5f06b95f2c33aa8a5b169933663d70292afa1
Santiago Cortes <santi.lamarin98@gmail.com>
2021-07-08 15:17:49 -0500

    Ejemplo Async Await en el ecamScript 8

--- src/ecm8/index.js
+++ src/ecm8/index.js
@@ -22,3 +22,31 @@ const string = 'Hello';
 console.log(string.padStart(7,'Hi'));
 console.log(string.padEnd(12,' --------'));
 console.log('Food'.padEnd(12,' ----------'));
+
+//=========================================
+//Async Await
+const helloWorld = () =>{
+    return new Promise((resolve,reject) =>{
+        (false)
+        ? setTimeout(() => resolve("Hello World"),3000)
+        : reject(new error('Test Error'))
+    })
+};
+
+const helloAsync = async () =>{
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () =>{
+     try{
+         const hello = await helloWorld();
+         console.log(hello);
+     }catch(error){
+         console.log(error);
+     }
+ }
+
+ anotherFunction();
